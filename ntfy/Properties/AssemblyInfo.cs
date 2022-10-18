ae8656b1d084c5fa45f7f403915581c7dbfac739
nharris <nharris@easypost.com>
2022-10-18 12:07:40 -0600

    - I think I understand nuspec and assembly info now...

---
+++ ntfy/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyProduct("ntfy")]  // Product ID
+[assembly: AssemblyTitle("ntfy")] // Human-friendly project name
+[assembly: AssemblyDescription("ntfy is a .NEY library for interacting with a ntfy server")]
+[assembly: AssemblyCopyright("Copyright © 2022")]
